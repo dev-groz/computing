a1f50231e47ed46523e1359d0075deba8c730a7e
dev-groz <groza4ka@gmail.com>
2023-10-11 23:09:16 +0700

    add new fuction to find error of function and polynom

--- task1.c
+++ task1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "printing.h"
 #include "lagrange.h"
@@ -19,9 +20,67 @@ double mysin(double x, int n){
     return result;
 }
 
+int factorial(int n){
+
+    int result = 1;
+
+    for (int i = 1; i <= n; i++){
+        result *= i;
+    }
+
+    return result;
+}
+
 double sin_of_square(double x){
     return mysin(x * x, 20);
 }
+
+double derivative(double xi, int n){
+    if (n == 1){
+        return 2 * xi;
+    }
+
+    if (n == 2){
+        return 2 + 2 * 2 * xi * xi;
+    }
+
+    return n * (n - 1) * derivative(xi, n - 2) + pow(2, n) * pow(xi, n);
+}
+
+double find_error(double* x, int n, double xi, double point){
+    double result = derivative(xi, n + 1);
+
+    result /= factorial(n + 1);
+
+    double omega = 1;
+    for (int i = 0; i < n; i++)
+    {
+        omega *= point - x[i];
+    }
+    
+    omega = omega > 0 ? omega : -omega;
+
+    return result;
+}
+
+void output_in_file_polynom(double* x, double* y, double* new_x, int n, int new_n, char* filename){
+    // FILE* file = fopen("output_lag.dat", "w");
+    FILE* file = fopen(filename, "w");
+
+    if (file == NULL){
+        exit(1);
+    }
+
+    for (int i = 0; i < new_n + 1; i++)
+    {
+        // double result = polynom_lagrange(x, y, n, new_x[i]);
+        double result = polynom_newton(x, y, n, new_x[i]);
+        fprintf(file, "%f\t %f\n", new_x[i], result);
+    }
+
+    fclose(file);
+}
+
 
 int main(){
     double a = -3.0;
@@ -50,21 +109,7 @@ int main(){
     }
 
 
-    // FILE* file = fopen("output_lag.dat", "w");
-    FILE* file = fopen("output_new.dat", "w");
-
-    if (file == NULL){
-        exit(1);
-    }
-
-    for (int i = 0; i < new_n + 1; i++)
-    {
-        // double result = polynom_lagrange(x, y, n, new_x[i]);
-        double result = polynom_newton(x, y, n, new_x[i]);
-        fprintf(file, "%f\t %f\n", new_x[i], result);
-    }
-
-    fclose(file);
+    // output_in_file_polynom(x, y, new_x, n, new_n, "output_lag.dat");
 
 
     // for (int i = 0; i < new_n + 1; i++)
@@ -81,6 +126,13 @@ int main(){
     // print_table(x, y, n);
     //print_by_rows(x, y, n);
     //output_in_file(x, y, n, "output.dat");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%f\t%f\n", x[i], find_error(x, 3, 4, x[i]));
+    }
+    
+
 
     free(x);
     free(y);
