188cec1f8cdaade9848bb22dc3042f3c2e6f52a3
dev-groz <groza4ka@gmail.com>
2025-02-18 22:26:26 +0700

    add jacobi method with animations

--- main.py
+++ main.py
@@ -4,21 +4,61 @@ from tkinter import ttk
 import numpy as np
 
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
-
-def u(X, Y):
+def real_u(X, Y):
     return np.exp(X) * np.cos(Y)
+
+def f(x, y):
+    return 0.2 * np.exp(x) * np.cos(y)
+
+ani = []
+
+def plot_jacobi_method(): 
+    global ani
+    h = float(entry_step.get())
+    tau = float(entry_step.get())
+
+    n = (int)(1 // h) + 1
+
+    a = 1
+    b = 1.2
+
+    u = np.zeros((n, n))
+    next_u = np.zeros((n, n))
+    for i in range(n):
+        next_u[i][0] = real_u(i*h, 0)
+        next_u[i][n-1] = real_u(i*h, 1)
+        next_u[0][i] = real_u(0, i*tau)
+        next_u[n-1][i] = real_u(1, i*tau)
+    
+    artists = []
+
+    fig = plt.figure(2)
+    plt.title("Метод Якоби")
+
+    for k in range(1000):
+        for i in range(1, n - 1):
+            for j in range(1, n - 1):
+                x_i = h * i
+                t_j = tau * j
+                next_u[i][j] = (f(x_i, t_j)*h*h*tau*tau + a*tau*tau*(u[i+1][j] + u[i-1][j]) + b*h*h*(u[i][j+1] + u[i][j-1])) / (2*(a*tau*tau + b*h*h))
+        u = next_u
+        artists.append([plt.imshow(u.T, origin='lower', cmap='coolwarm', extent=(0,1,0,1))])
+
+    ani = animation.ArtistAnimation(fig, artists=artists, interval=100)
+
+    plt.show()
 
 def plot_real_function():
     step = float(entry_step.get())
     X = np.arange(0, 1, step)
     Y = np.arange(0, 1, step)
     X, Y = np.meshgrid(X, Y)
-    Z = u(X, Y)
+    Z = real_u(X, Y)
 
-    plt.imshow(Z, origin='lower', cmap=cm.coolwarm, extent=(0,1,0,1))
+    plt.figure(1)
+    plt.imshow(Z, origin='lower', cmap='coolwarm', extent=(0,1,0,1))
     
     plt.title("Точное решение")
 
@@ -57,5 +97,6 @@ label_max_eigen.grid(column = 0, row = 3)
 label_max_eigen_value = ttk.Label(frm, text='0')
 label_max_eigen_value.grid(column = 1, row = 3)
 
-ttk.Button(frm, text='Показать точное решение', command=plot_real_function).grid(column = 2, row = 5)
+ttk.Button(frm, text='Показать точное решение', command=plot_real_function).grid(column = 1, row = 5)
+ttk.Button(frm, text='Метод Якоби', command=plot_jacobi_method).grid(column = 2, row = 5)
 root.mainloop()
