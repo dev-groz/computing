bb27ce4b5765ccbc6f759d3c88fbd994204cfe07
dev-groz <groza4ka@gmail.com>
2025-02-24 22:03:06 +0700

    add method for max eigen value

--- main.py
+++ main.py
@@ -12,6 +12,58 @@ def real_u(X, Y):
 def f(x, y):
     return 0.2 * np.exp(x) * np.cos(y)
 
+def max_eigen_value():
+    h = float(entry_step.get())
+    tau = float(entry_step.get())
+
+    n = (int)(1 // h) + 1
+
+    a = 1
+    b = 1.2
+
+    eps = float(entry_error.get())
+
+    u = np.ones((n, n))
+    for i in range(n):
+        u[i][0] = 0
+        u[i][n-1] = 0
+        u[0][i] = 0
+        u[n-1][i] = 0
+
+    u_norm = 0    
+    for i in range(n):
+        for j in range(n):
+            u_norm += u[i][j] * u[i][j]
+    u_norm = u_norm ** 0.5
+
+    u = u / u_norm
+
+    v = np.empty((n, n))
+
+    prev_eigen = float('inf')
+    eigen_value = 0
+
+    while abs(prev_eigen - eigen_value) >= eps:
+        for i in range(1, n - 1):
+            for j in range(1, n - 1):
+                v[i][j] = -a*(u[i+1][j] - 2 * u[i][j] + u[i-1][j])/(h*h) - b*(u[i][j+1] - 2 * u[i][j] + u[i][j-1])/(tau*tau)
+        
+        prev_eigen = eigen_value
+        eigen_value = 0
+        for i in range(n):
+            for j in range(n):
+                eigen_value += u[i][j] * v[i][j]
+        
+        v_norm = 0    
+        for i in range(n):
+            for j in range(n):
+                v_norm += v[i][j] * v[i][j]
+        v_norm = v_norm ** 0.5
+        u = v / v_norm
+        label_max_eigen_value['text'] = str(eigen_value)
+
+
+
 ani = []
 
 def plot_jacobi_method(): 
@@ -99,4 +151,5 @@ label_max_eigen_value.grid(column = 1, row = 3)
 
 ttk.Button(frm, text='Показать точное решение', command=plot_real_function).grid(column = 1, row = 5)
 ttk.Button(frm, text='Метод Якоби', command=plot_jacobi_method).grid(column = 2, row = 5)
+ttk.Button(frm, text='Найти максимальное собственное значение', command=max_eigen_value).grid(column = 1, row = 6)
 root.mainloop()
